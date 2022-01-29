022fee4cd2db719fa09ffa56b95a4f83aac82398
Md Saquib Shakeel <saquib.shakeel.1866@gmail.com>
2022-01-29 20:20:19 +0530

    Time: 1 ms (44.77%), Space: 42.4 MB (10.90%) - LeetHub

---
+++ 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.java
@@ -0,0 +1,37 @@
+class Solution {
+    public int search(int[] nums, int target) {
+        int pivot = findPivot(nums);
+        if(pivot == -1)return search(nums, 0, nums.length-1, target);
+        if(nums[pivot]==target)return pivot;
+        if(nums[0]<=target)return search(nums, 0, pivot-1, target);
+        return search(nums, pivot+1, nums.length-1, target);
+    }
+    
+    int findPivot(int[] arr){
+        int s = 0;
+        int e = arr.length-1;
+        while(s<=e){
+            int m = s + (e-s)/2;
+            if(m<e && arr[m]>arr[m+1])return m;
+            if(m>s && arr[m]<arr[m-1])return m-1;
+            if(arr[s]>=arr[m]){
+                e=m-1;
+            }
+            else{
+                s=m+1;
+            }
+        }
+        return -1;
+    }
+    
+    int search(int[] arr, int s, int e, int t){
+        if(s<=e){
+            int m = s + (e - s)/2;
+            if(arr[m]==t)return m;
+            else if(arr[m]>t)return search(arr, s, m-1, t);
+            else return search(arr, m+1, e, t);
+        }
+        return -1;
+    }
+    
+}

16c4b06b35df359dbc0879b2a79270be427cc89a
Md Saquib Shakeel <saquib.shakeel.1866@gmail.com>
2022-01-30 20:15:43 +0530

    Time: 2 ms (42.45%), Space: 64.4 MB (5.22%) - LeetHub

---
+++ 189-rotate-array/189-rotate-array.java
@@ -0,0 +1,27 @@
+class Solution {
+    public void rotate(int[] nums, int k) {
+        if(k>=nums.length)k=k-nums.length;
+        if(nums.length==2 && k>0){
+            // if(nums.length==2){
+            //     int a=0;
+            //     while(a<k){
+                    reverse(nums, 0, 1);
+            //         a++;
+            //     }
+        }
+        if(nums.length>2 && k>0){
+            reverse(nums, 0, nums.length-1);
+            reverse(nums, 0, k-1);
+            reverse(nums, k, nums.length-1);
+        }
+    }
+    
+    void reverse(int[] arr, int s, int e){
+        while(s<e){
+            int temp = arr[s];
+            arr[s++] = arr[e];
+            arr[e--] = temp;
+        }
+    }
+    
+}

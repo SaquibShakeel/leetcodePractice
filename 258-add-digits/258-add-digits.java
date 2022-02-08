d70ea180d3cd5708102f545c6b514dc46a5d504d
Md Saquib Shakeel <saquib.shakeel.1866@gmail.com>
2022-02-08 18:20:59 +0530

    Time: 1 ms (98.89%), Space: 41.3 MB (9.91%) - LeetHub

--- 258-add-digits/258-add-digits.java
+++ 258-add-digits/258-add-digits.java
@@ -1,19 +1,5 @@
 class Solution {
     public int addDigits(int num) {
-        // while(Math.log10(num)>=1){
-        //     num = add(num);
-        // }
-        // return num;
         return num - 9*((num-1)/9);
     }
-    
-//     int add(int n){
-//         int ans = 0;
-//         while(n > 0){
-//             ans += n%10;
-//             n = n/10;
-//         }
-//         return ans;
-//     }
-    
 }

10c3f59080277eff7cdfb2bc36eaf68ba7f2065a
Md Saquib Shakeel <saquib.shakeel.1866@gmail.com>
2022-02-09 22:21:42 +0530

    Time: 10 ms (58.98%), Space: 45.4 MB (38.48%) - LeetHub

--- 532-k-diff-pairs-in-an-array/532-k-diff-pairs-in-an-array.java
+++ 532-k-diff-pairs-in-an-array/532-k-diff-pairs-in-an-array.java
@@ -13,9 +13,21 @@ class Solution {
         return ans;
     }
     
+    // boolean search(int[] arr, int t, int skip){
+    //     for(int i=0; i<arr.length; i++){
+    //         if(arr[i] == t && i!=skip)return true;
+    //     }
+    //     return false;
+    // }
+    
     boolean search(int[] arr, int t, int skip){
-        for(int i=0; i<arr.length; i++){
-            if(arr[i] == t && i!=skip)return true;
+        int s=0;
+        int e=arr.length-1;
+        while(s<=e){
+            int m = s + (e - s)/2;
+            if(arr[m]==t && m!=skip)return true;
+            else if(arr[m]>t)e=m-1;
+            else s=m+1;
         }
         return false;
     }

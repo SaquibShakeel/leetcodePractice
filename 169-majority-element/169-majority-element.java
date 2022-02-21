735dd9ffc6cd8a79baf2f728ab76d785baf3c973
Md Saquib Shakeel <saquib.shakeel.1866@gmail.com>
2022-02-21 23:24:07 +0530

    Time: 670 ms (5.05%), Space: 46.1 MB (72.41%) - LeetHub

--- 169-majority-element/169-majority-element.java
+++ 169-majority-element/169-majority-element.java
@@ -1,20 +1,21 @@
 class Solution {
     public int majorityElement(int[] nums) {
-        if(quantity(nums, nums[0]) > nums.length/2)return nums[0];
+        if(quantity(nums, nums[0]))return nums[0];
         for(int i=1; i<(nums.length+1)/2; i++){
-            if(nums[i] != nums[i-1] && quantity(nums, nums[i]) > nums.length/2){
+            if(nums[i] != nums[i-1] && quantity(nums, nums[i])){
                 return nums[i];
             }
         }
         return -1;
     }
     
-    int quantity(int[] arr, int n){
+    boolean quantity(int[] arr, int n){
         int ans = 0;
         for(int e : arr){
             if(e==n)ans++;
         }
-        return ans;
+        if(ans > arr.length/2)return true;
+        return false;
     }
     
 }

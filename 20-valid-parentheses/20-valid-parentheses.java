f5ef6ddf4f4368406443e39ecf923c77c889e83d
Md Saquib Shakeel <saquib.shakeel.1866@gmail.com>
2022-04-30 00:03:10 +0530

    Time: 3 ms (48.36%), Space: 41.8 MB (62.73%) - LeetHub

---
+++ 20-valid-parentheses/20-valid-parentheses.java
@@ -0,0 +1,33 @@
+class Solution {
+    public boolean isValid(String s) {
+        
+        if(s.length()%2 != 0) return false;
+        double count = 0;
+        
+        List<Character> c = new ArrayList<Character>();
+        for(int i = 0; i < s.length(); i++) {
+            if(s.charAt(i) == '(' || s.charAt(i) == '{' || s.charAt(i) == '['){
+                c.add(s.charAt(i));
+                count++;
+            }
+            else {
+                if(c.size() == 0) return false;
+                if(s.charAt(i) == ')') {
+                    if(c.get(c.size()-1) == '(') c.remove(c.size()-1);
+                    else return false;
+                }
+                else if(s.charAt(i) == '}') {
+                    if(c.get(c.size()-1) == '{') c.remove(c.size()-1);
+                    else return false;
+                }
+                else if(s.charAt(i) == ']') {
+                    if(c.get(c.size()-1) == '[') c.remove(c.size()-1);
+                    else return false;
+                }
+            }
+        }
+        if(c.size() != 0 && count != s.length()/2.0) return false;
+        return true;
+    }
+    
+}

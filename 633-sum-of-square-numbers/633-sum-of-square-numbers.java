059b161ff66405365eb9146586e6feb3d86c6ac0
Md Saquib Shakeel <saquib.shakeel.1866@gmail.com>
2022-06-28 03:55:16 +0530

    Time: 4 ms (87.28%), Space: 39.3 MB (86.62%) - LeetHub

---
+++ 633-sum-of-square-numbers/633-sum-of-square-numbers.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean judgeSquareSum(int c) {
+        long a = 0;
+        long b = (int)Math.sqrt(c);
+        while(a<=b){
+            if(a*a + b*b == c)return true;
+            else if(a*a + b*b < c)a++;
+            else {
+                b--;
+                a--;
+            }
+        }
+        return false;
+    }
+}

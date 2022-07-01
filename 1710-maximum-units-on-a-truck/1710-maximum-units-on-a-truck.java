678ea71ccced7db9f34fda9351699b740f0a704c
Md Saquib Shakeel <saquib.shakeel.1866@gmail.com>
2022-07-02 00:43:42 +0530

    Time: 50 ms (5.09%), Space: 50.9 MB (52.67%) - LeetHub

--- 1710-maximum-units-on-a-truck/1710-maximum-units-on-a-truck.java
+++ 1710-maximum-units-on-a-truck/1710-maximum-units-on-a-truck.java
@@ -1,50 +1,19 @@
 class Solution {
     public int maximumUnits(int[][] boxTypes, int truckSize) {
-        // int n = boxTypes.length;
-        // int i = 0;
-        // int j = 1;
-        // while(i < n-1) {
-        //     if(boxTypes[i][1] >= boxTypes[j][1]) {
-        //         j++;
-        //     }else {
-        //         swap(boxTypes, i, j);
-        //         j++;
-        //     }
-        //     if(j >= n) {
-        //         i++;
-        //         j = i+1;
-        //     }
-        // }
-        // int ans = 0;
-        // int box = 0;
-        // i = 0;
-        // while (i < n && box <= truckSize) {
-        //     if(boxTypes[i][0] <= truckSize - box) {
-        //         ans += boxTypes[i][0] * boxTypes[i][1];
-        //         box += boxTypes[i][0];
-        //     }else{
-        //         ans += (truckSize - box) * boxTypes[i][1];
-        //         box += truckSize - box;
-        //     }
-        //     i++;
-        // }
-        // return ans;
-        
-        Arrays.sort(boxTypes, Comparator.comparingInt(o -> -o[1]));
-		int ans = 0, i = 0, n = boxTypes.length;
-		while (i < n && truckSize > 0) {
-			int maxi = Math.min(boxTypes[i][0], truckSize);
-			ans += maxi * boxTypes[i][1];
-			i++;
-			truckSize -= maxi;
-		}
-		return ans;
+        Arrays.sort(boxTypes, Comparator.<int[]>comparingInt(value -> value[1]).reversed());
+        int max = 0;
+        int currentCapacity = 0;
+        int index = 0;
+        while (index < boxTypes.length && currentCapacity < truckSize) {
+            final int[] boxType = boxTypes[index];
+            if (boxType[0] == 0) {
+                index++;
+            } else {
+                max += boxType[1];
+                currentCapacity++;
+                boxType[0] -= 1;
+            }
+        }
+        return max;
     }
-    
-//     void swap(int[][] arr, int f, int s) {
-//         int[] temp = arr[f];
-//         arr[f] = arr[s];
-//         arr[s] = temp;
-//     }
-    
 }
